1c83445856bf4a45a02849cb32ea0b72d961939c
basicxd <mikail-fener@hotmail.com>
2019-05-09 12:21:56 +0200

    Working

---
+++ RESTNødopkald/Sensor.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.InteropServices.ComTypes;
+
+namespace RESTNødopkald
+{
+    public class Sensor
+    {
+        public string Dato { get; set; }
+        public string Tid { get; set; }
+        public string Motion { get; set; }
+
+        public Sensor() { }
+
+        public Sensor(string dato, string tid, string motion)
+        {
+            Dato = dato;
+            Tid = tid;
+            Motion = motion;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Dato)}: {Dato}, {nameof(Tid)}: {Tid}, {nameof(Motion)}: {Motion}";
+        }
+    }
+}
